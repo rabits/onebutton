0fcc4ee2f1e763a213a257aa46431c1402b4d607
Rabit <home@rabits.org>
2016-10-29 00:48:13 +0300

    Added Guitarix web interface start while starting up

--- src/Guitarix.py
+++ src/Guitarix.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
+import httplib
+from os import path
+
 from jsonrpctcp import config as JSONRPCConfig, connect as JSONRPCConnect
+from websockify import WebSocketProxy
 
 import Log as log
 from Process import Process
 
 class Guitarix(Process):
     """Guitarix controller"""
+    _webprocess = None
 
     def start(self):
         JSONRPCConfig.append_string = '\n'
@@ -18,9 +23,20 @@ class Guitarix(Process):
         Process.start(self)
 
     def _clientConnect(self):
-        log.info("Connecting Guitarix client '%s'" % self._cfg['name'])
+        log.info("Connecting client '%s'" % self._cfg['name'])
         self._client = JSONRPCConnect('localhost', self._cfg['rpc_port'])
         self.version()
+
+        if self._cfg['web']:
+            self._webprocess = GuitarixWeb(self._cfg, self._logout, self._logerr, self._pidpath + '.web.pid')
+            self._webprocess.wait()
+
+    def stop(self):
+        log.info("Stopping '%s'" % self._cfg['name'])
+        if self._webprocess:
+            self._webprocess.stop()
+            del self._webprocess
+        Process.stop(self)
 
     def version(self):
         return self._client.getversion()
@@ -56,3 +72,31 @@ class Guitarix(Process):
         my_outs = jack.getPorts(name_pattern='%s_amp:out_0' % self.name(), is_audio=True, is_output=True)
         my_ins = jack.getPorts(name_pattern='%s_fx:in_0' % self.name(), is_audio=True, is_input=True)
         jack.connectPorts(my_outs[0], my_ins[0])
+
+class GuitarixWeb(Process):
+    """GuitarixWeb UI interface and web proxy
+    Unable to use threading or multiprocessing with websockify...
+    """
+
+    def start(self):
+        base_dir = path.dirname(path.dirname(__file__))
+        proxy_path = path.join(base_dir, 'lib', 'websockify', 'websocketproxy.py')
+        webui_path = path.join(base_dir, 'guitarix-webui')
+        self._command = ['python', '-u', proxy_path,
+                '--web', webui_path,
+                '*:%d' % self._cfg['web'],
+                'localhost:%d' % self._cfg['rpc_port']]
+        Process.start(self)
+
+    def _clientConnect(self):
+        log.info("Checking GuitarixWeb UI '%s'" % self._cfg['name'])
+        self._client = httplib.HTTPConnection('localhost', self._cfg['web'], timeout=10)
+        self._client.request('GET', '/')
+        r = self._client.getresponse()
+        r.close()
+        if r.status != 200:
+            raise Exception("Unable to connect GuitarWeb UI")
+
+    def stop(self):
+        log.info("Stopping web proxy for '%s'" % self._cfg['name'])
+        Process.stop(self)
